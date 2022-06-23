236488f41350177598e3997e468646067e86cefe
Maciej <mswiatek0@gmail.com>
2022-06-23 21:25:46 +0200

    window functions

--- SQL_scripts.sql
+++ SQL_scripts.sql
@@ -527,6 +527,122 @@ SELECT custid
 FROM sales.OrderValues
 
 
+-----------------
+
+
+SELECT report.custid
+	,report.orderid
+	,convert(decimal(10,2),report.val / report.custTotalVal * 100) AS OrderPercentOfCustVal
+	,convert(decimal(10,2),report.custTotalVal / report.totalVal * 100) AS CustPercentOfTotalVal
+FROM (
+	SELECT orderid
+		,custid
+		,val
+		,sum(val) OVER () AS totalVal
+		,sum(val) OVER (PARTITION BY custid) AS custTotalVal
+	FROM sales.OrderValues
+	) AS report
+
+------------
+
+
+SELECT empid
+	,ordermonth
+	,val
+	,sum(val) OVER (PARTITION BY empid ORDER BY ordermonth)
+FROM sales.EmpOrders
+
+-------------
+drop table if exists dbo.Orders
+go
+CREATE TABLE dbo.Orders (
+	orderid INT NOT NULL
+	,orderdate DATE NOT NULL
+	,empid INT NOT NULL
+	,custid VARCHAR(15) NOT NULL
+	,qty INT NOT NULL
+	,CONSTRAINT PK_Orders PRIMARY KEY (orderid)
+	)
+
+insert into dbo.Orders
+values
+(30001, '20140802',3,'A',10),
+(10001, '20141224',2,'A',12),
+(10005,'20141224',1,'B',20),
+(40001, '20150109',2,'A',40),
+(10006,'20140118',1,'C',14),
+(20001,'20150212',2,'B',12),
+(40005,'20160212',3,'A',10),
+(20002,'20160216',1,'C',20),
+(30003, '20160418',2,'B',15),
+(30004,'20140418',3,'C',22),
+(30007,'20160907',3,'D',30)
+
+
+-------------------
+
+SELECT custid
+	,orderid
+	,rank() OVER (PARTITION BY custid ORDER BY qty) AS rnk
+	,DENSE_RANK() OVER (PARTITION BY custid ORDER BY qty) AS drnk
+FROM dbo.Orders
+
+
+
+select * from dbo.Orders
+
+
+
+SELECT ord.custid
+	,ord.orderid
+	,ord.qty
+	,ord.qty - ord.prevorder AS diffprev
+	,ord.qty - ord.nextorder AS diffnext
+FROM (
+	SELECT custid
+		,orderid
+		,orderdate
+		,qty
+		,lag(qty) OVER (ORDER BY custid,orderdate) AS prevorder
+		,lead(qty) OVER (ORDER BY custid,orderdate) AS nextorder
+	FROM dbo.Orders
+	) AS ord
+
+
+------------
+
+select * from dbo.Orders
+
+SELECT empid
+	,COUNT(CASE WHEN year(orderdate) = 2014	THEN orderid ELSE NULL END) AS cnt2014
+	,COUNT(CASE	WHEN year(orderdate) = 2015	THEN orderid ELSE NULL END) AS cnt2015
+	,COUNT(CASE	WHEN year(orderdate) = 2016	THEN orderid ELSE NULL END) AS cnt2016
+FROM dbo.Orders
+GROUP BY empid
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
